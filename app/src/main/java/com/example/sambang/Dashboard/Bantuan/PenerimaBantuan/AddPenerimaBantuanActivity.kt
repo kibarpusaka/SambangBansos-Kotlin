c26dd1593b628f8216776695c832cb610f9efdcc
kibarpusaka <tuple1004@gmail.com>
2021-07-08 22:51:44 +0700

    FiturPenerimaBantuan

---
+++ app/src/main/java/com/example/sambang/Dashboard/Bantuan/PenerimaBantuan/AddPenerimaBantuanActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sambang.Dashboard.Bantuan.PenerimaBantuan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddPenerimaBantuanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_penerima_bantuan)
+    }
+}
