79ed32fec0571a08038bdbdf3c769d9ccdbda864
Prieyudha Akadita S <akaditasustono@gmail.com>
2021-07-10 16:14:24 +0700

    add local search desa

--- app/src/main/java/com/example/sambang/Dashboard/Master/Desa/Presenter/DesaPresenter.kt
+++ app/src/main/java/com/example/sambang/Dashboard/Master/Desa/Presenter/DesaPresenter.kt
@@ -1,6 +1,7 @@
 package com.example.sambang.Dashboard.Master.Desa.Presenter
 
 import com.example.sambang.Api.SambangUtils
+import com.example.sambang.Dashboard.Master.Desa.Data.ModelDesaMaster
 import com.example.sambang.Dashboard.Master.Desa.Data.ResponDesa
 import com.example.sambang.Login.Data.ModelLogin
 import com.example.sambang.SessionManager
@@ -9,6 +10,16 @@ import retrofit2.Callback
 import retrofit2.Response
 
 class DesaPresenter(val dataDesaView: DataDesaView) {
+    private val villages = mutableListOf<ModelDesaMaster>()
+
+    fun showAllData(){
+        dataDesaView.onSuccessDataDesa(villages)
+    }
+
+    fun filterData(q: String){
+        val filtered = villages.filter { v -> v.nama!!.toLowerCase().contains(q.toLowerCase()) }
+        dataDesaView.onSuccessDataDesa(filtered)
+    }
 
     fun getDataDesa(user : ModelLogin?){
         val sessionManager : SessionManager? = null
@@ -18,6 +29,8 @@ class DesaPresenter(val dataDesaView: DataDesaView) {
                 override fun onResponse(call: Call<ResponDesa>, response: Response<ResponDesa>) {
                     val body = response.body()
                     if (body?.status == true) {
+                        villages.clear()
+                        villages.addAll(body.data_desa)
                         dataDesaView.onSuccessDataDesa(body.data_desa)
                     }else {
                         dataDesaView.onErrorDataDesa(body?.message)
